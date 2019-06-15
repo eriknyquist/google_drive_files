a24d6bf636512f9a5a39d6848d0bec8169178346
Erik <eknyquist@gmail.com>
2019-06-15 12:47:09 -0700

    Updates, seems to be working

--- google_drive_files.py
+++ google_drive_files.py
@@ -4,21 +4,31 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 SECRETS_DIR = os.path.join(os.path.expanduser('~'), '.google_drive')
-SECRETS_FILE = os.path.join(SECRETS_DIR, "client_secrets.json")
-CREDS_FILE = os.path.join(SECRETS_DIR, "client_credentials.txt")
+SECRETS_FILE = "client_secrets.json"
+CREDS_FILE = "credentials.txt"
+SECRETS_PATH = os.path.join(SECRETS_DIR, SECRETS_FILE)
+CREDS_PATH = os.path.join(SECRETS_DIR, CREDS_FILE)
+LIST_CMD = {'q': "'root' in parents and trashed=false"}
+
 
 class Downloader(object):
+    """
+    Wrapper class to simplify authentication & file listing with Google Drive
+    using PyDrive
+    """
+
     def __init__(self):
         self._drive = None
 
-        if not os.path.exists(SECRETS_FILE):
+        if not os.path.exists(SECRETS_PATH):
             if not os.path.exists(SECRETS_DIR):
                 os.mkdir(SECRETS_DIR)
 
-            raise RuntimeError("cannot find file %s" % SECRETS_FILE)
+            raise RuntimeError("Please put a client_secrets.json file in %s"
+                               % SECRETS_DIR)
         
         gauth = GoogleAuth()
-        gauth.LoadCredentialsFile(CREDS_FILE)
+        gauth.LoadCredentialsFile(CREDS_PATH)
         if gauth.credentials is None:
             gauth.LocalWebserverAuth()
         elif gauth.access_token_expired:
@@ -26,16 +36,50 @@ class Downloader(object):
         else:
             gauth.Authorize()
 
-        gauth.SaveCredentialsFile(CREDS_FILE)
+        gauth.SaveCredentialsFile(CREDS_PATH)
         self._drive = GoogleDrive(gauth)
 
+    def get_file(self, filename):
+        """
+        Download a file from authenticated google drive account by filename
+
+        :param str filename: name of file to download
+        :return: True if file was downloaded successfully, False otherwise
+        :rtype: bool
+        """
+
+        if self._drive is None:
+            raise RuntimeError("Not authenticated")
+
+        file_list = self._drive.ListFile(LIST_CMD).GetList()
+        for filedata in file_list:
+            if filedata['title'] == filename:
+                try:
+                    filedata.GetContentFile(filename)
+                except Exception as e:
+                    print(e)
+                    return False
+
+                return True
+
+        return False
+
     def file_listing(self):
+        """
+        Get a list of the names of files available to download
+
+        :return: list of filenames available for download
+        :rtype: list
+        """
+
         if self._drive is None:
             raise RuntimeError("Not authenticated")
 
         file_list = self._drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
-        for file1 in file_list:
-            print('title: %s, id: %s' % (file1['title'], file1['id']))
+        return [f['title'] for f in file_list]
 
-d = Downloader()
-d.file_listing()
+
+if __name__ == "__main__":
+    d = Downloader()
+    for filename in d.file_listing():
+        print filename
